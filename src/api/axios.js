62e3226d8eb7d341762c0741426377b3e713c7b1
jaehun <gnsl2725@naver.com>
2024-12-04 12:09:42 +0900

    feat: axios.js 파일 추가
    기본 URL 및 공통 헤더 설정 포함

---
+++ src/api/axios.js
@@ -0,0 +1,13 @@
+import axios from "axios";
+
+const apiKey = process.env.REACT_APP_API_KEY;
+
+const instance = axios.create({
+  baseURL: "https://api.themoviedb.org/3",
+  params: {
+    api_key: apiKey,
+    language: "ko-KR",
+  },
+});
+
+export default instance;
